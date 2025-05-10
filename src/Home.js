2deb86e18e1d6efd968ec3ce570de7e212f94cef
olalekan <olalekanotuyiga@gmail.com>
2025-05-10 16:28:38 +0100

    handling of error

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,33 @@ import Bloglist from "./Bloglist";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch data for that resources");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          // console.log(err.message);
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <Bloglist blogprops={blogs} title="ALL for Bloger" />}
 
