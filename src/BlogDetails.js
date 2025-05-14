a171579cf5274a46293fea9e793fe25278551af3
olalekan <olalekanotuyiga@gmail.com>
2025-05-14 19:24:26 +0100

    continue styling

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./usefetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    datas: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
-    <div className="blog-detail">
-      <h2>detailss of blogs {id}</h2>
+    <div className="blog-details">
+      {isPending && <div>loading...</div>}
+      {error && <div>{error}</div>}
+      {blogs && (
+        <article>
+          <h2>{blogs.title}</h2>
+          <p>Written by {blogs.author}</p>
+          <div>{blogs.body}</div>
+        </article>
+      )}
     </div>
   );
 };
