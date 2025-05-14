2e43b13621630e52de21cb17786e2dc655e241cc
olalekan <olalekanotuyiga@gmail.com>
2025-05-14 16:39:10 +0100

    adding route params

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Bloglist = (props) => {
   const blogss = props.blogprops;
   const titless = props.title;
@@ -7,8 +8,11 @@ const Bloglist = (props) => {
       <h2>{titless}</h2>
       {blogss.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+          </Link>
+
           {/* <button onClick={() => handleDeleting(blog.id)}>Delete button</button> */}
         </div>
       ))}
